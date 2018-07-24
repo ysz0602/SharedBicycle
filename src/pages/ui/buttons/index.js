018081c567ea00a3165c72d3575ac0fcba87509a
ysz <1582945670@qq.com>
2018-07-24 22:33:13 +0800

    按钮组件

--- src/pages/ui/buttons/index.js
+++ src/pages/ui/buttons/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { Card, Button } from 'antd';
+import { Card, Button, Radio } from 'antd';
 import './index.less';
 
 export default class Buttons extends React.Component{
-
+  state = {
+    loading: true,
+    size: "default"
+  }
+  handleCloseClick = () => {
+    this.setState({
+      loading: false
+    })
+  }
+  handleChange = (e) => {
+    this.setState({
+      size: e.target.value
+    })
+  }
   render(){
     return(
       <div>
-        <Card title="基础按钮">
+        <Card title="基础按钮" className="card-wrap">
           <Button type="primary">yangsz</Button>
           <Button>yansz</Button>
           <Button type="dashed">yangsz</Button>
           <Button type="danger">yangsz</Button>
           <Button disabled>yangsz</Button>
         </Card>
-        <Card title="图形按钮">
+        <Card title="图形按钮" className="card-wrap">
           <Button icon="plus">创建</Button>
           <Button icon="edit">编辑</Button>
           <Button icon="delete">删除</Button>
@@ -22,12 +35,29 @@ export default class Buttons extends React.Component{
           <Button type="primary" icon="search">搜索</Button>
           <Button type="primary" icon="download">下载</Button>
         </Card>
-        <Card title="Loading按钮">
-          <Button type="primary" loading={true}>确定</Button>
-          <Button type="primary" shape="circle" loading={true}></Button>
-          <Button loading={true}>点击加载</Button>
-          <Button shape="circle" loading={true}></Button>
-          <Button type="primary">关闭</Button>
+        <Card title="Loading按钮" className="card-wrap">
+          <Button type="primary" loading={this.state.loading}>确定</Button>
+          <Button type="primary" shape="circle" loading={this.state.loading}></Button>
+          <Button loading={this.state.loading}>点击加载</Button>
+          <Button shape="circle" loading={this.state.loading}></Button>
+          <Button type="primary" onClick={this.handleCloseClick}>关闭</Button>
+        </Card>
+        <Card title="按钮组">
+          <Button.Group>
+            <Button type="primary" icon="left" style={{marginRight: 0}}>返回</Button>
+            <Button type="primary" icon="right">前进</Button>
+          </Button.Group>
+        </Card>
+        <Card title="按钮尺寸" className="card-wrap">
+          <Radio.Group value={this.state.size} onChange={this.handleChange}>
+            <Radio value="small">小</Radio>
+            <Radio value="default">中</Radio>
+            <Radio value="large">大</Radio>
+          </Radio.Group>
+          <Button type="primary" size={this.state.size}>yangsz</Button>
+          <Button size={this.state.size}>yansz</Button>
+          <Button type="dashed" size={this.state.size}>yangsz</Button>
+          <Button type="danger" size={this.state.size}>yangsz</Button>
         </Card>
       </div>
     );
