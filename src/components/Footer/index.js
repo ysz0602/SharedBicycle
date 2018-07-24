69757d81204f5570445d57c29391baa6fd933c5b
yangshangzhao <yangshangzhao@yangshangzhao.com>
2018-07-24 11:20:37 +0800

    提交项目

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.less';
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        版权所有：慕课网&河畔一角（推荐使用谷歌浏览器，可以获得更佳的操作页面）。
+      </div>
+    );
+  }
+}
