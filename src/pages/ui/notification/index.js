cb9c0f2bc4fba9176dedc7e2d87611a1970bdf32
yangshangzhao <yangshangzhao@yangshangzhao.com>
2018-07-25 13:25:36 +0800

    notification

--- src/pages/ui/notification/index.js
+++ src/pages/ui/notification/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import { Card, Button, notification  } from 'antd';
 
 export default class Notification extends React.Component{
-  openNotification = (type) => {
+  openNotification = (type, direction) => {
+    if(direction){
+      notification.config({
+        placement:direction,
+        message: '发工资了',
+        description: '上个月考勤22天，迟到12天，实发工资250.'
+      })
+    }
     notification[type]({
       message: '发工资了',
       description: '上个月考勤22天，迟到12天，实发工资250.'
@@ -16,6 +23,13 @@ export default class Notification extends React.Component{
           <Button type="primary" onClick={() => this.openNotification('info')}>Info</Button>
           <Button type="primary" onClick={() => this.openNotification('warning')}>Warning</Button>
           <Button type="primary" onClick={() => this.openNotification('error')}>Error</Button>
+        </Card>
+
+        <Card title="通知提醒框2" className="card-wrap">
+          <Button type="primary" onClick={() => this.openNotification('success','topLeft')}>Success</Button>
+          <Button type="primary" onClick={() => this.openNotification('info','topRight')}>Info</Button>
+          <Button type="primary" onClick={() => this.openNotification('warning','bottomLeft')}>Warning</Button>
+          <Button type="primary" onClick={() => this.openNotification('error','bottomRight')}>Error</Button>
         </Card>
       </div>
     )
