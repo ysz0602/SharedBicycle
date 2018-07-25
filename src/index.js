b2cd316e874446b745157b5c904b2f31578b6108
yangshangzhao <yangshangzhao@yangshangzhao.com>
2018-07-25 11:04:42 +0800

    注释未用到的引入

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Admin from './admin';
+// import Admin from './admin';
 import Router from './router';
 import registerServiceWorker from './registerServiceWorker';
 
